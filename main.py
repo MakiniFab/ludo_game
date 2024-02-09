e4fba8a665ca0ea1d90d3b8c25bcc507826724d7
makinifab <makinifarbean@gmail.com>
2024-02-09 08:53:01 -0800

    set_display

---
+++ main.py
@@ -0,0 +1,16 @@
+import pygame
+import random
+
+# initializing the game
+pygame.init()
+
+#set up display
+WIDTH , HEIGHT = 600, 600
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Mountain Top")
+
+#colors
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+WHITE = (0, 255, 255)
+WHITE = (0, 0, 255)
