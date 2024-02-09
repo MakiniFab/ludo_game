179ac36cd21650676b83711636274a678b14d969
makinifab <makinifarbean@gmail.com>
2024-02-09 09:04:09 -0800

    main_game_run_loop

--- main.py
+++ main.py
@@ -20,5 +20,19 @@ BLACK = (0, 0, 0)
 #game variables
 player_position = {RED: 0, GREEN: 0, BLUE: 0, YELLOW: 0}
 player_colors = [RED, GREEN, BLUE, YELLOW]
+current_player = 0
 dice_value = 1
 
+#main game loop
+running = True
+while running:
+    screen.fill(WHITE)
+
+    for eb=vent in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                dice_value = random.randint(1, 6)
+                player_position[player_colors[current_player]] += dice_value
+                current_player = (current_player + 1) % 4
